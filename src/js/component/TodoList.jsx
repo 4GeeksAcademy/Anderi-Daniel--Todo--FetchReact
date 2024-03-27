686b5fcabe461836cba230b863e632580dce8e51
Daniel Anderi <100786824+danderi@users.noreply.github.com>
2024-03-27 03:08:33 +0000

    finished with styles

--- src/js/component/TodoList.jsx
+++ src/js/component/TodoList.jsx
@@ -1,4 +1,5 @@
 import React, { useState, useEffect } from 'react';
+import { FcEmptyTrash } from "react-icons/fc";
 
 const TodoList = () => {
     const [task, setTask] = useState("");
@@ -26,14 +27,14 @@ const TodoList = () => {
 
     const handleInputEnter = async (e) => {
         if (e.key === "Enter" && task.trim() !== "") {
-            const updatedList = [...list, { label: task, done: false }]; 
+            const updatedList = [...list, { label: task, done: false }];
             setList(updatedList)
             try {
                 const response = await fetch(
                     `https://playground.4geeks.com/apis/fake/todos/user/Daniel1`,
                     {
                         method: 'PUT',
-                        body: JSON.stringify(updatedList), 
+                        body: JSON.stringify(updatedList),
                         headers: {
                             'Content-Type': 'application/json',
                         },
@@ -42,52 +43,48 @@ const TodoList = () => {
                 if (!response.ok) {
                     throw new Error('Failed to update todo list');
                 }
-                if(response.ok){
+                if (response.ok) {
                     console.log('Todo list added and updated successfully');
                 }
-                
             } catch (error) {
                 console.error('Error updating todo list:', error);
             }
-
-            setTask(""); 
+            setTask("");
         }
     }
 
     const handleDeleteTask = async (label) => {
         let newList = list.filter((item) => item.label !== label);
         setList(newList);
-            try {
-                const response = await fetch(
-                    `https://playground.4geeks.com/apis/fake/todos/user/Daniel1`,
-                    {
-                        method: 'PUT',
-                        body: JSON.stringify(newList), 
-                        headers: {
-                            'Content-Type': 'application/json',
-                        },
-                    }
-                );
-                if (!response.ok) {
-                    throw new Error('Failed to update todo list');
+        try {
+            const response = await fetch(
+                `https://playground.4geeks.com/apis/fake/todos/user/Daniel1`,
+                {
+                    method: 'PUT',
+                    body: JSON.stringify(newList),
+                    headers: {
+                        'Content-Type': 'application/json',
+                    },
                 }
-                if(response.ok){
-                    console.log('Todo list task deleted and updated successfully');
-                }
-                
-            } catch (error) {
-                console.error('Error updating todo list:', error);
+            );
+            if (!response.ok) {
+                throw new Error('Failed to update todo list');
             }
-
+            if (response.ok) {
+                console.log('Todo list task deleted and updated successfully');
+            }
+        } catch (error) {
+            console.error('Error updating todo list:', error);
+        }
     }
 
     return (
-        <div className="notebook">
+        <div className="TodoWrapper">
             <h1>To Do List</h1>
-            <input onKeyDown={handleInputEnter} onChange={e => setTask(e.target.value)} value={task} placeholder='add task here' type='text'></input>
-            <ol>
-                {list.map((item, index) => (<li key={index}>{item.label}<button onClick={()=> handleDeleteTask(item.label)}>X</button></li>))}
-            </ol>
+            <input className="todo-input" onKeyDown={handleInputEnter} onChange={e => setTask(e.target.value)} value={task} placeholder='Add task here' type='text'></input>
+            <ol >
+                {list.map((item, index) => (<li className="Todo" key={index}>{item.label}<button className="color" onClick={() => handleDeleteTask(item.label)}><FcEmptyTrash className="color"/></button></li>))}
+            </ol >
         </div >
     )
 }
